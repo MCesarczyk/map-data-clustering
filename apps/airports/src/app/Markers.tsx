0458ba4fffefb18d13e71e3ed66bbe6d40969fd2
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-05-08 02:21:28 +0200

    refactor(airports): rename example app properly

--- apps/airports/src/app/Markers.tsx
+++ apps/airports/src/app/Markers.tsx
@@ -2,9 +2,9 @@
 import { useEffect } from 'react';
 import { useMap } from 'react-leaflet';
 import L from 'leaflet';
-import "leaflet.markercluster/dist/leaflet.markercluster";
-import "leaflet.markercluster/dist/MarkerCluster.css";
-import "leaflet.markercluster/dist/MarkerCluster.Default.css";
+import 'leaflet.markercluster/dist/leaflet.markercluster';
+import 'leaflet.markercluster/dist/MarkerCluster.css';
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 import { markers } from '@mdc/data';
 
 export const Markers = () => {
