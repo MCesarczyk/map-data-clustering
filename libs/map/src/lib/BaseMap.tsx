4a7cadb0f31540c3155d6f259d5de864870c4c1c
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2024-05-08 03:02:51 +0200

    feat: add basic react app (#1)
    * build: bootstrap first react app
    
    * build: create library for shared data
    
    * feat(map): create lib with reusable map instance
    
    * feat(simple): create basic app presenting clusterisation on airports example
    
    * refactor(airports): rename example app properly
    
    * refactor(map): make component reusable by passing api key as prop
    
    * docs(readme): update

---
+++ libs/map/src/lib/BaseMap.tsx
@@ -0,0 +1,37 @@
+import { type ReactNode } from 'react';
+import { MapContainer } from 'react-leaflet';
+import VectorBasemapLayer from 'react-esri-leaflet/plugins/VectorBasemapLayer';
+import { LatLngTuple } from 'leaflet';
+
+const initialPosition = [51.505, -0.09] satisfies LatLngTuple;
+const initialZoom = 13;
+const mapStyle = 'OSM:DarkGray';
+
+interface BaseMapProps {
+  apiKey: string;
+  center?: LatLngTuple;
+  zoom?: number;
+  height?: number;
+  children?: ReactNode;
+}
+
+export const BaseMap = ({
+  apiKey,
+  center,
+  zoom,
+  height,
+  children,
+}: BaseMapProps) => {
+  return (
+    <MapContainer
+      style={{ minHeight: `${height || 500}px`, width: '100%', zIndex: 100 }}
+      center={center || initialPosition}
+      zoom={zoom || initialZoom}
+      maxZoom={20}
+      scrollWheelZoom={true}
+    >
+      <VectorBasemapLayer apiKey={apiKey} name={mapStyle} />
+      {children}
+    </MapContainer>
+  );
+};
